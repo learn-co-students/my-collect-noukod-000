ac257b7f0b38f15f36e0d0bf490b2938dfc203b1
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-08 20:44:36 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+collection_arr_list = []
 
+def my_collect(collection)
+  counter = 0
+  new_arr_collection = []
+  
+  while counter < collection.length
+    new_arr_collection << yield(collection[counter])
+    counter += 1
+  end
+    new_arr_collection
+end
 
+my_collect(collection_arr_list) do |container|
+  container.split(" ").first
+end
