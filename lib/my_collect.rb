728502863880ce35aeb74696b1106205e2acf084
Jhana Gustave <jhana.gustave@gmail.com>
2019-04-24 16:02:23 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(array)
+  # code here
+  i = 0
+   collection = []
+    while i < array.length
+    collection << yield(array[i])
+    i += 1
+    end
+   return collection
+          array = ["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoine Miller"]
+          my_collect(array) do |name|
+           name.split(" ").first
+    end
+end
